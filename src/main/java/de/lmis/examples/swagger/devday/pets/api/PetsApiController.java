45345334d06901ca68a8dec8ef59e0cdf4052a6e
Sascha Doemer <Sascha.Doemer@LMIS.de>
2017-12-02 11:10:01 +0100

    Corrections.

--- src/main/java/de/lmis/examples/swagger/devday/pets/api/PetsApiController.java
+++ src/main/java/de/lmis/examples/swagger/devday/pets/api/PetsApiController.java
@@ -1,12 +1,6 @@
 package de.lmis.examples.swagger.devday.pets.api;
 
-import de.lmis.examples.swagger.devday.pets.model.Pet;
-import de.lmis.examples.swagger.devday.pets.model.Pets;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-
-import java.util.List;
 
 @Controller
 public class PetsApiController implements PetsApi {
